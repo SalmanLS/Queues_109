fde183312d03fc46085616752fc8fa30d9209df5
SalmanLS <salmanluthfi456@gmail.com>
2022-12-12 16:37:46 +0700

    initializing the values to indicate the queue is empty

--- CircularQueues/Program.cs
+++ CircularQueues/Program.cs
@@ -6,5 +6,14 @@ namespace CircularQueues
     {
         int FRONT, REAR, max = 5;
         int[] queue_array = new int[5];
+
+        public Program()
+        {
+            //initializing the values of the variable REAR and FRONT to -1 to indicate that queue is initially empty.
+            FRONT = -1;
+            REAR = -1;
+        }
+
+
     }
 }
