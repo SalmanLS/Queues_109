1af9f277eff45c1adbcecc2ebe87d51b981cb95e
SalmanLS <salmanluthfi456@gmail.com>
2022-12-12 17:05:31 +0700

    create display method and declare new int variable

--- CircularQueues/Program.cs
+++ CircularQueues/Program.cs
@@ -72,5 +72,12 @@ namespace CircularQueues
             }
         }
 
+        public void display()
+        {
+            int FRONT_position = FRONT;
+            int REAR_position = REAR;
+            
+        }
+
     }
 }
