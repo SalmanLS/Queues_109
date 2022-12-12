e1bd66267295caac5762608cc9682d00360f6646
SalmanLS <salmanluthfi456@gmail.com>
2022-12-12 17:04:10 +0700

    remove method if the queue has one element and if the element is at the last position.

--- CircularQueues/Program.cs
+++ CircularQueues/Program.cs
@@ -53,7 +53,23 @@ namespace CircularQueues
                 return;
             }
             Console.WriteLine("\n The element deleted from the queue is: " + queue_array[FRONT] + "\n");
-            
+            /* Check if the queue has one element. */
+            if (FRONT == REAR)
+            {
+                FRONT = -1;
+                REAR = -1;
+            }
+            else
+            {
+                /* if the element to be deleted is at the last position of the array, then the value of FRONT is set to 0 i.e to 
+                 the first element of the array. */
+                if (FRONT == max -1)
+                    FRONT= 0;
+                else
+                    /*FRONT is increamented by one if it is not the first element of array. */
+                    FRONT = FRONT + 1;
+
+            }
         }
 
     }
