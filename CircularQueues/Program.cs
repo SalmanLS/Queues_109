ecb1229872e2377fbb128b4f9120ef1066d0cb5e
SalmanLS <salmanluthfi456@gmail.com>
2022-12-12 17:35:18 +0700

    create main to run the app, and the menu, and the switch case

--- CircularQueues/Program.cs
+++ CircularQueues/Program.cs
@@ -113,5 +113,57 @@ namespace CircularQueues
             }
         }
 
+        static void Main(string[] args)
+        {
+            Program queue = new Program();
+            char ch;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Menu");
+                    Console.WriteLine("1. Implement insert operation");
+                    Console.WriteLine("2. Implement delete operation");
+                    Console.WriteLine("3. Display values");
+                    Console.WriteLine("4. Exit");
+                    Console.Write("\n Enter your choice (1-4): ");
+                    ch = Convert.ToChar(Console.Read());
+                    Console.WriteLine();
+                    switch (ch)
+                    {
+                        case '1':
+                            {
+                                Console.WriteLine("Enter a number: ");
+                                int num = Convert.ToInt32(Console.ReadLine());
+                                Console.WriteLine();
+                                queue.insert(num);
+                            }
+                            break;
+                        case '2':
+                            {
+                                queue.remove();
+                            }
+                            break;
+                        case '3':
+                            {
+                                queue.display();
+                            }
+                            break;
+                        case '4':
+                            return;
+                        default:
+                            {
+                                Console.WriteLine("Invalid option!!");
+                            }
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Check for the values entered.");
+                }
+            }
+        }
+
     }
 }
